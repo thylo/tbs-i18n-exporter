0922f21e8b45e5e37b74184e895915329d90a2a0
Julien Moreau <julien@thylo.be>
2025-07-23 09:06:52 +0200

    complete refresh of the out folder
    # Conflicts:
    #	src/app.js

--- src/app.js
+++ src/app.js
@@ -1,52 +1,61 @@
-import { writeFileSync, readFileSync } from "fs";
+import { writeFileSync, existsSync, mkdirSync } from "fs";
 import { resolve } from "path";
+import { rimraf } from "rimraf";
 
 const url =
-  "https://script.google.com/macros/s/AKfycbx03qzmQrIwJTyrIJRXSZTM4FFICzKn0mDOE10ItYw-eXFTynARdyE80t4QKMwlowgD/exec"; // WAE12
-  //"https://script.google.com/macros/s/AKfycbzMsMOqOzEIx-GJPRohqCt2Fz7P5QkmEw62ZK-yteyoE5AEIFqcRATscewowqbtGA6C/exec"; // WAE34
-
-const translationKeys = readFileSync(
-  resolve("./in/Translation Keys.txt"),
-  "utf8",
-)
-  .split("\n")
-  .map((v) => v.trim())
-  .filter((v) => v);
+  "https://script.google.com/macros/s/AKfycbx03qzmQrIwJTyrIJRXSZTM4FFICzKn0mDOE10ItYw-eXFTynARdyE80t4QKMwlowgD/exec";
 
 function fetchData() {
   return fetch(url).then((res) => res.json());
 }
 
 function writeLang(lang, data) {
-  const currentValues = readFileSync(
-    resolve(`./in/Text (${lang.toUpperCase()}).txt`),
-    "utf8",
-  ).split("\n");
-  const values = Object.keys(data).reduce(
-    (acc, key) => {
-      const row = data[key];
-      const value = row[lang].replaceAll("\n", "\\n").replaceAll(/\x03/g, "");
-      const index = translationKeys.indexOf(key);
-      console.log(lang, key, index, value);
-      acc[index] = value;
-      return acc;
-    },
-    new Array(translationKeys.length)
-      .fill("")
-      .map((v, i) => currentValues[i] || "empty"),
-  );
+  console.log("writing lang", lang);
+
+  const values = Object.keys(data).reduce((acc, key) => {
+    const row = data[key];
+    const value = row[lang]
+      .trim()
+      .replaceAll("\n", "\\n")
+      .replaceAll(/\x03/g, "");
+    return [...acc, value];
+  }, []);
   const path = resolve(`./out/Text (${lang.toUpperCase()}).txt`);
   writeFileSync(path, values.join("\n"));
 }
 
-fetchData().then((data) => {
-  const langs = [
-    ...new Set(
-      Object.keys(data)
-        .map((key) => data[key])
-        .map((key) => Object.keys(key))
-        .flat(),
-    ),
-  ];
-  langs.forEach((lang) => writeLang(lang, data));
+const clean = () => rimraf("./out/**", { glob: true });
+
+const ensureFolderExists = (folderPath) => {
+  if (!existsSync(folderPath)) {
+    mkdirSync(folderPath, { recursive: true });
+    console.log(`Folder created: ${folderPath}`);
+  } else {
+    console.log(`Folder already exists: ${folderPath}`);
+  }
+};
+
+const createTranslationKeys = (data) => {
+  const path = resolve(`./out/Translation Keys.txt`);
+  const values = Object.keys(data)
+  writeFileSync(path, values.join("\n"));
+}
+
+const init = () =>
+  fetchData().then((data) => {
+    const langs = [
+      ...new Set(
+        Object.keys(data)
+          .map((key) => data[key])
+          .map((key) => Object.keys(key))
+          .flat(),
+      ),
+    ];
+    createTranslationKeys(data);
+    langs.forEach((lang) => writeLang(lang, data));
+  });
+
+clean().then(() => {
+  ensureFolderExists('./out');
+  init()
 });
